85ae219842c230b9d520d8ae0727e86e41618539
madhavakondapalli402 <127126539+madhavakondapalli402@users.noreply.github.com>
2023-05-04 12:31:40 +0530

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,3 +1,6 @@
+JAVA_HOME='/opt/jdk-20.0.1'
+PATH="$JAVA_HOME/bin:$PATH"
+export PATH
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
